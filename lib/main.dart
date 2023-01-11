d3ecc8e461cf4208ab25a9e4cea7b5edc61f3da0
Undateable <8910dante@gmail.com>
2023-01-11 15:52:03 -0300

    Suff
    #Suffering

--- lib/main.dart
+++ lib/main.dart
@@ -7,36 +7,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'Tack',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Silicon-Aid: Tack'),
-
-
-
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -45,19 +31,43 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+class Change extends StatefulWidget{
+  const Change({
+    Key key,
+    this.color = const Color(0xFFFFE306),
+  }) : super(key: key);
+  final Color color;
 
+  _ChangeState createState() => _ChangeState();
+}
+
+class _ChangeState extends State<Change>{
+  double size =1.0;
+  void grow(){
+    setState(() {
+      size += 0.1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: widget.color,
+      transform: Matrix4.diagonal3Values(size, size, 1.0),
+    );
+  }
+}
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-
   void _incrementCounter() {
+
     setState(() {
+
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
@@ -66,6 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
